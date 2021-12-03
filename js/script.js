78c121ad1faad64442657b7f6a8fc9c7b668ace3
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-03 14:04:19 +0100

    add script to generate random numbers

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ Consigli del giorno:
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 */
 
+let numbers = getRandomIntInclusive(1, 100);
+console.log(numbers);
+
+divMessage = document.querySelector('.message');
+divMessage.innerHTML = `<h3>I numeri random da memorizzare sono:</h3>
+                        <h2>${numbers.join(', ')}</h2>
+                        <h3> Hai 30 secondi per memorizzarli.</h3>`;
+
+
+//funzione per creare numeri random nell'array blackList
+function getRandomIntInclusive(min, max) {
+
+    //creo array vuoto
+    let numbers = [];
+
+    //ciclo for per inserire 5 numeri unici nell'array
+    for (let index = 0; index < 5; index++) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        //The maximum is inclusive and the minimum is inclusive
+        let element = numbers[index];
+        element = Math.floor(Math.random() * (max - min + 1) + min);
+
+        //controllo se il numero esiste già nell'array
+        while (numbers.includes(element)) {
+            element = Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        numbers.push(element);
+
+    }
+    return numbers;
+}
